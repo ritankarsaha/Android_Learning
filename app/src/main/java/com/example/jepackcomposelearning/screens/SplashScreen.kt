80e954a105b11eea6463cfa6583788208ff55dcf
RITANKAR SAHA <ritankar.saha786@gmail.com>
2025-12-18 23:52:49 +0530

    Splash Screen updated imports

--- app/src/main/java/com/example/jepackcomposelearning/screens/SplashScreen.kt
+++ app/src/main/java/com/example/jepackcomposelearning/screens/SplashScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jepackcomposelearning.R
 
 @Composable
