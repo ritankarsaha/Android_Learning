05bd39592b721ac0273fd0d99335644ca5464c11
RITANKAR SAHA <ritankar.saha786@gmail.com>
2025-12-24 16:48:05 +0530

    Final Design of the Home Page Jetpack Compose

--- app/src/main/java/com/example/jepackcomposelearning/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/jepackcomposelearning/screens/WelcomeScreen.kt
@@ -3,12 +3,21 @@ package com.example.jepackcomposelearning.screens
 import com.example.jepackcomposelearning.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,8 +25,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WelcomeScreen(modifier: Modifier = Modifier) {
@@ -55,6 +67,54 @@ fun WelcomeScreen(modifier: Modifier = Modifier) {
                     contentDescription = null,
                     modifier = modifier.size(160.dp)
                 )
+            }
+
+        }
+
+        // splitting the screen using the weight parameters
+        Column(modifier = Modifier.fillMaxWidth()
+            .weight(0.35f)
+            .padding(horizontal = 24.dp, vertical = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Text("Get the Freshest Fruits Here",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF272140),
+                    textAlign = TextAlign.Center,
+                    lineHeight = 28.sp
+                )
+
+                // providing the space between the two components properly
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text("I am Ritankar Saha, hehe",
+                    fontSize = 16.sp,
+                    color = Color(0xFF50577E),
+                    textAlign = TextAlign.Center,
+                    lineHeight = 24.sp
+                )
+
+                Spacer(modifier = Modifier.height(36.dp))
+
+                Button(onClick = {}, modifier = modifier.fillMaxWidth()
+                    .height(56.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF8C42)),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+
+                    Text("Tap on Me Ahh !! ",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.White
+                    )
+
+                }
+
             }
 
         }
